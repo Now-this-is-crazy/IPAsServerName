eab13248e577d05e179d868257e1b051d4012ae7
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-02-26 11:36:04 +0200

    feat: update mod config

--- src/main/java/io/github/redvortexdev/iasn/IPAsServerName.java
+++ src/main/java/io/github/redvortexdev/iasn/IPAsServerName.java
@@ -6,17 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class IPAsServerName implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("iasn");
+	public static final String MOD_ID = "iasn";
+	public static final String MOD_NAME = "IPAsServerName";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info(MOD_NAME + " Initialized!");
 	}
 }
